60167472755b57c614350aff2d14f558ed435971
AFarinacci <andreafarinacci@me.com>
2020-06-17 14:25:09 -0500

    add search ability

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -24,6 +24,8 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
   addTrack(track) {
     if (
@@ -46,6 +48,13 @@ class App extends React.Component {
   updatePlaylistName(name) {
     this.setState({ playlistName: name });
   }
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map((track) => track.id);
+    return trackURIs;
+  }
+  search(term) {
+    console.log(term);
+  }
   render() {
     return (
       <div>
@@ -53,7 +62,7 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults
               searchResults={this.state.searchResults}
@@ -64,6 +73,7 @@ class App extends React.Component {
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
